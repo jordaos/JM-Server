b80042c6d3b124330360a575aadf4d8a3e0cef3e
Jordão Macedo <jordao05@hotmail.com>
2017-12-28 15:05:12 -0300

    Config database access

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_fde0baffb1f6a11'),
+            'username' => env('DB_USERNAME', 'b92049836177ed'),
+            'password' => env('DB_PASSWORD', '9aaae5a3'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
