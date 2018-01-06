6931170eea93246c4b0375090f93403e439fbde3
Jordão Macedo <jordao05@hotmail.com>
2018-01-06 16:39:55 -0300

    ass secure_assets when load css

--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'description', 'technologies', 'photos'];
+    protected $fillable = ['title', 'description', 'technologies'];
 
     public function images() {
         return $this->hasMany('App\Image', 'project_id');
