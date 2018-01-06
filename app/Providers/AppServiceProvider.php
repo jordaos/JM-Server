0ccce6098f171e365e4c1241ce0da1bba5b06653
Jordão Macedo <jordao05@hotmail.com>
2018-01-06 16:45:57 -0300

    add secure_assets when load css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(env('APP_ENV') == 'production') {
-            \URL::forceScheme('https');
-        }
     }
 
     /**
